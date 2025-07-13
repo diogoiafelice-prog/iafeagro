b27bf1c08b6a36b36ab111eeaff01fd2a0f2425e
CHAN Shu <59114727+CHANShu0508@users.noreply.github.com>
2025-07-13 19:01:26 +0800

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © CHEN Shu.
+      <time>{YEAR}</time> © TT.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
